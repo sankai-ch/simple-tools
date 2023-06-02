7849a3c5770cd92fed185597c1df2d7b6c69defb
山海 <260830911@qq.com>
2023-06-02 11:10:54 +0800

    项目初始化

---
+++ src/test/java/com/sankai/st/StApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sankai.st;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
