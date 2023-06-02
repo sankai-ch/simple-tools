febda55fa565dc81de595050a1cfb2a65bdbf314
山海 <260830911@qq.com>
2023-06-02 20:51:51 +0800

    正常运行项目构建

--- src/main/java/com/sankai/st/StApplication.java
+++ src/main/java/com/sankai/st/StApplication.java
@@ -1,13 +1,32 @@
 package com.sankai.st;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class StApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StApplication.class, args);
+	private static final Logger log = LoggerFactory.getLogger(StApplication.class);
+
+	public static void main(String[] args) throws UnknownHostException {
+		SpringApplication springApplication = new SpringApplication(StApplication.class);
+		ConfigurableEnvironment environment = springApplication.run(args).getEnvironment();
+
+		log.info("\n----------------------------------------------------------\n\t"
+						+ "Application'{}' is running! Access URLs:\n\t"
+						+ "Local:\t\thttp://localhost:{}\n\t"
+						+ "External: \thttp://{}:{}\n\t----------------------------------------------------------",
+				environment.getProperty("spring.application.name"),
+				environment.getProperty("server.port"),
+				InetAddress.getLocalHost().getHostAddress(),
+				environment.getProperty("server.port"));
+
 	}
 
 }
